3c74f69551060e13d052ac6df9d5b2bded9eea43
MeLoBeats <lamelobeats@gmail.com>
2023-11-30 10:28:14 +0100

    add barcode scanner

--- App.js
+++ App.js
@@ -1,53 +1,103 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button, StyleSheet } from 'react-native'; 
-import CameraScreen from './screens/Camera';
-import HomeScreen from './screens/HomeScreen';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { Button, StyleSheet } from 'react-native'; 
+// import CameraScreen from './screens/Camera';
+// import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
+
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-        name="Home" 
-        component={HomeScreen} 
-        options={{
-          title: 'My home',
-          headerStyle: {
-            backgroundColor: '#f4511e',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-          headerRight: () => (
-            <Button
-              onPress={() => alert('This is a button!')}
-              title="Info"
-              color="#000"
-            />
-          ),
-        }}
-        />
-        <Stack.Screen name="Camera" component={CameraScreen} options={{ title: 'Camera Page' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center'
+    padding: 20,
   },
-  textInput: {
-    borderColor: 'black',
-    borderWidth: 1,
-    width: '80%',
-    padding: 10,
-    marginBottom: 10
-  }
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
 });
+
+
+// export default function App() {
+
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen 
+//         name="Home" 
+//         component={HomeScreen} 
+//         options={{
+//           title: 'My home',
+//           headerStyle: {
+//             backgroundColor: '#f4511e',
+//           },
+//           headerTintColor: '#fff',
+//           headerTitleStyle: {
+//             fontWeight: 'bold',
+//           },
+//           headerRight: () => (
+//             <Button
+//               onPress={() => alert('This is a button!')}
+//               title="Info"
+//               color="#000"
+//             />
+//           ),
+//         }}
+//         />
+//         <Stack.Screen name="Camera" component={CameraScreen} options={{ title: 'Camera Page' }} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center'
+//   },
+//   textInput: {
+//     borderColor: 'black',
+//     borderWidth: 1,
+//     width: '80%',
+//     padding: 10,
+//     marginBottom: 10
+//   }
+// });
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Button } from "react-native"
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button onPress={() => navigation.navigate('Camera')}>Scannez un QR Code</Button>
     </View>
   )
 }
